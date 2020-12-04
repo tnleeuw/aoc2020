75c021ec29c44e75df62c80e4f29fae121b243fa
Tim van der Leeuw <tim.vanderleeuw@hiber.global>
2020-12-04 12:30:26 +0100

    Day 4, puzzle 2. Fix an embarrassing mistake in field-validation that I didn't catch (or test for) in first pass.

--- src/main/kotlin/org/tnl/aoc2020/day4.kt
+++ src/main/kotlin/org/tnl/aoc2020/day4.kt
@@ -12,20 +12,39 @@ object Day4Puzzle1 {
         println("Nr of valid passports: $nrValid")
     }
 
-    fun countValidPassports(passports: List<Passport>): Int {
-        return passports.count { it.isValidForPuzzle1() }
+    fun readPassports(fileName: String): List<Passport> =
+        readPassports(fileName, ::isValidFieldPuzzle1)
+
+}
+
+object Day4Puzzle2 {
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val passports = readPassports("day4-data.txt")
+        val nrValid = countValidPassports(passports)
+
+        println("Nr of valid passports: $nrValid")
     }
+
+    fun readPassports(fileName: String): List<Passport> =
+        readPassports(fileName, ::isValidFieldPuzzle2)
+
 }
 
 typealias Passport = Map<String, String>
 
-fun readPassports(fileName: String): List<Passport> {
-    return readPassports(getDataInputStream(fileName)
-        .bufferedReader()
-        .lines().asSequence())
+fun countValidPassports(passports: List<Passport>): Int {
+    return passports.count { it.isValidForPuzzle1() }
 }
 
-fun readPassports(lines: Sequence<String>, isValidField: (String, String) -> Boolean = ::isValidField): List<Passport> {
+fun readPassports(fileName: String, isValidField: (String, String) -> Boolean): List<Passport> {
+    return readPassports(getDataInputStream(fileName)
+        .bufferedReader()
+        .lines().asSequence(), isValidField)
+}
+
+fun readPassports(lines: Sequence<String>, isValidField: (String, String) -> Boolean): List<Passport> {
     val sequence = sequence<Passport> {
         var passport = mutableMapOf<String, String>()
         lines.forEach {  line ->
@@ -42,8 +61,43 @@ fun readPassports(lines: Sequence<String>, isValidField: (String, String) -> Boo
 }
 
 val VALID_FIELDS = setOf("byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid", "cid")
-fun isValidField(field: String, value: String): Boolean {
+fun isValidFieldPuzzle1(field: String, value: String): Boolean {
     return field in VALID_FIELDS
+}
+
+val VALID_YEAR_RE = "\\d{4}".toRegex()
+val VALID_HEX_COLOR = "#[a-f0-9]{6}".toRegex()
+val VALID_PID = "\\d{9}".toRegex()
+val VALID_HEIGHT_RE = "(\\d+)(cm|in)".toRegex()
+val VALID_EYE_COLOURS = setOf("amb", "blu", "brn", "gry", "grn", "hzl", "oth")
+fun isValidYear(value: String, min: Int, max: Int): Boolean {
+    return VALID_YEAR_RE.matches(value) && value.toInt() in min..max
+}
+
+fun isValidHeight(value: String): Boolean {
+    val matchResult = VALID_HEIGHT_RE.matchEntire(value) ?: return false
+
+    val (_, height, units) = matchResult.groupValues
+    return when(units) {
+        "cm" -> height.toInt() in 150..193
+        "in" -> height.toInt() in 59..76
+        else -> false
+    }
+}
+
+val FIELD_VALIDATIONS = mapOf(
+    "byr" to { value: String -> isValidYear(value, 1920, 2002) },
+    "iyr" to { value: String -> isValidYear(value, 2010, 2020) },
+    "eyr" to { value: String -> isValidYear(value, 2020, 2030) },
+    "hgt" to { value: String -> isValidHeight(value) },
+    "hcl" to { value: String -> VALID_HEX_COLOR.matches(value) },
+    "ecl" to { value: String -> value in VALID_EYE_COLOURS },
+    "pid" to { value: String -> VALID_PID.matches(value) },
+    "cid" to { value: String -> true },
+)
+fun isValidFieldPuzzle2(field: String, value: String): Boolean {
+    val validator = FIELD_VALIDATIONS[field] ?: return false
+    return validator(value)
 }
 
 fun Passport.isValidForPuzzle1(): Boolean {
@@ -51,7 +105,7 @@ fun Passport.isValidForPuzzle1(): Boolean {
             || (size == 7 && !containsKey("cid"))
 }
 
-val PASSPORT_FIELD_RE = "([a-z]{3}:.+?)\\s*".toRegex()
+val PASSPORT_FIELD_RE = "([a-z]{3}:[#a-zA-Z0-9]+)\\s*".toRegex()
 internal fun extractFieldsFromLine(
     passport: MutableMap<String, String>,
     line: String,
@@ -59,7 +113,7 @@ internal fun extractFieldsFromLine(
 ) {
     PASSPORT_FIELD_RE.findAll(line).forEach { matchResult ->
         matchResult.groupValues.forEach { entry ->
-            val (field, value) = entry.split(":")
+            val (field, value) = entry.trim().split(":")
             if (isValidField(field, value)) {
                 passport.put(field, value)
             }
--- src/test/kotlin/org/tnl/aoc2020/day4test.kt
+++ src/test/kotlin/org/tnl/aoc2020/day4test.kt
@@ -2,6 +2,7 @@ package org.tnl.aoc2020
 
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
@@ -23,7 +24,7 @@ internal class ReadPassportTest {
     )
     internal fun testIsValidField(field: String, value: String, expected: Boolean) {
         // Act
-        val result = isValidField(field, value)
+        val result = isValidFieldPuzzle1(field, value)
 
         // Assert
         assertEquals(expected, result)
@@ -51,7 +52,7 @@ internal class ReadPassportTest {
         val passport = mutableMapOf<String, String>()
 
         // Act
-        extractFieldsFromLine(passport, line, ::isValidField)
+        extractFieldsFromLine(passport, line, ::isValidFieldPuzzle1)
 
         // Assert
         assertEquals(expectedNrOfFields, passport.size)
@@ -64,7 +65,7 @@ internal class ReadPassportTest {
         val lines = getDataInputStream("day4-test.txt").bufferedReader().lines().asSequence()
 
         // Act
-        val passports = readPassports(lines)
+        val passports = readPassports(lines, ::isValidFieldPuzzle1)
 
         // Assert
         assertEquals(4, passports.size)
@@ -79,7 +80,7 @@ internal class ReadPassportTest {
     )
     internal fun testValidPassports(passportNr: Int, expected: Boolean) {
         // Arrange
-        val passports = readPassports("day4-test.txt")
+        val passports = Day4Puzzle1.readPassports("day4-test.txt")
 
         // Act
         val result = passports[passportNr].isValidForPuzzle1()
@@ -91,12 +92,104 @@ internal class ReadPassportTest {
     @Test
     internal fun testCountValidPassports() {
         // Arrange
-        val passports = readPassports("day4-test.txt")
+        val passports = Day4Puzzle1.readPassports("day4-test.txt")
 
         // Act
-        val nrValid = Day4Puzzle1.countValidPassports(passports)
+        val nrValid = countValidPassports(passports)
 
         // Assert
         assertEquals(2, nrValid)
+    }
+}
+
+internal class Puzzle2FieldValidationsTest {
+
+    @DisplayName("{index} {0} value {1} expected: {2}")
+    @ParameterizedTest(name = "{index} {0} value {1} expected: {2}")
+    @CsvSource(
+        "byr, 2002, true",
+        "byr, 2003, false",
+        "hgt, 60in, true",
+        "hgt, 190cm, true",
+        "hgt, 190in, false",
+        "hgt, 190, false",
+        "hcl, #123abc, true",
+        "hcl, #123abz, false",
+        "ecl, brn, true",
+        "ecl, wat, false",
+        "pid, 000000001, true",
+        "pid, 0123456789, false",
+        "cid, wat, true",
+        "byr, 1919, false",
+        "byr, 1920, true",
+        "iyr, 2009, false",
+        "iyr, 2010, true",
+        "iyr, 2020, true",
+        "iyr, 2021, false",
+        "eyr, 2019, false",
+        "eyr, 2020, true",
+        "eyr, 2030, true",
+        "eyr, 2031, false",
+        "ecl, amb, true",
+        "ecl, blu, true",
+        "ecl, gry, true",
+        "ecl, grn, true",
+        "ecl, hzl, true",
+        "ecl, oth, true",
+        "hgt, 6ft, false",
+        "hgt, 149cm, false",
+        "hgt, 150cm, true",
+        "hgt, 193cm, true",
+        "hgt, 194cm, false",
+        "hgt, 58in, false",
+        "hgt, 59in, true",
+        "hgt, 76in, true",
+        "hgt, 77in, false",
+        "hcl, 123456, false",
+        "hcl, #abc, false",
+        "pid, 1234, false",
+        "kwd, val, false",
+    )
+    internal fun testFieldValidations(field: String, value: String, expected: Boolean) {
+        // Act
+        val result = isValidFieldPuzzle2(field, value)
+
+        // Assert
+        assertEquals(expected, result) {
+            "Field '$field' value '$value' should match: $expected"
+        }
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        "ecl:gry pid:860033327 eyr:2020 hcl:#fffffd, 4, eyr",
+        "hcl:#cfa07d byr:1929, 2, byr"
+    )
+    internal fun testExtractFieldsFromLine(line: String, expectedNrOfFields: Int, fieldToCheck: String) {
+        // Arrange
+        val passport = mutableMapOf<String, String>()
+
+        // Act
+        extractFieldsFromLine(passport, line, ::isValidFieldPuzzle2)
+
+        // Assert
+        assertEquals(expectedNrOfFields, passport.size)
+        assertTrue(passport.containsKey(fieldToCheck))
+    }
+
+
+    @ParameterizedTest
+    @CsvSource(
+        "day4-test.txt, 2",
+        "day4p2-invalidpassports.txt, 0",
+        "day4p2-validpassports.txt, 4"
+    )
+    internal fun testCountValidPassports(fileName: String, expected: Int) {
+        // Act
+        val passports = Day4Puzzle2.readPassports(fileName)
+        val result = countValidPassports(passports)
+
+        // Assert
+        assertEquals(expected, result)
     }
 }
