aed85e1f5def14f3c215c4c49cd9d90de9ef8d1f
Tim van der Leeuw <tim.vanderleeuw@hiber.global>
2020-12-13 21:29:32 +0100

    Day 13 puzzle 2 - brute forcing it! :(

--- src/main/kotlin/org/tnl/aoc2020/day13.kt
+++ src/main/kotlin/org/tnl/aoc2020/day13.kt
@@ -1,5 +1,7 @@
 package org.tnl.aoc2020
 
+import java.math.BigInteger
+
 
 object Day13Puzzle1 {
     @JvmStatic
@@ -15,6 +17,20 @@ object Day13Puzzle1 {
         val shortestWait = findShortestWait(waitingTimes)
 
         return shortestWait.key * shortestWait.value
+    }
+}
+
+object Day13Puzzle2 {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val answer = calculateAnswer("day13-data.txt", BigInteger("100000000000000"))
+        println("The answer is: $answer")
+    }
+
+    fun calculateAnswer(fileName: String, startingAt: BigInteger): BigInteger {
+        val buses = readInputsPuzzle2(fileName)
+        val maxBus = buses.busWithLargestId()
+        return puzzle2BruteForcing(firstCandidateAfter(startingAt, maxBus), maxBus, buses)
     }
 }
 
@@ -40,3 +56,38 @@ fun calculateWaitingTime(arrival: Long, bus: Long): Long =
 fun findShortestWait(busWaitTimes: Map<Long, Long>): Map.Entry<Long, Long> =
     busWaitTimes.entries.minByOrNull { e -> e.value } ?: throw IllegalArgumentException("Cannot find shortest wait time")
 
+fun readInputsPuzzle2(fileName: String): Map<Int, BigInteger> {
+    val inp = getDataInputStream(fileName).bufferedReader()
+    // Discard first line
+    inp.readLine()
+    // Process only the 2nd line
+    return parseBusIdsWithIndex(inp.readLine())
+}
+
+fun parseBusIdsWithIndex(line: String): Map<Int, BigInteger> =
+    line.split(",")
+        .mapIndexed { index, s ->  Pair(index, s)}
+        .filter { it.second != "x" }
+        .map { (index, s) -> Pair(index, BigInteger.valueOf(s.toLong())) }
+        .associate { it }
+
+fun isGoldenTimestamp(timestamp: BigInteger, buses: Map<Int, BigInteger>): Boolean =
+    buses.all { (index, busId)  -> (timestamp + BigInteger.valueOf(index.toLong())) % busId == BigInteger.ZERO}
+
+tailrec fun puzzle2BruteForcing(testValue: BigInteger, steppingWithBus: Map.Entry<BigInteger, BigInteger>, buses: Map<Int, BigInteger>): BigInteger =
+    if (isGoldenTimestamp(testValue.testValueToTimestamp(steppingWithBus), buses)) testValue.testValueToTimestamp(steppingWithBus)
+    else puzzle2BruteForcing(testValue + steppingWithBus.value, steppingWithBus, buses)
+
+fun firstCandidateAfter(timestamp: BigInteger, bus: Map.Entry<BigInteger, BigInteger>): BigInteger =
+    ((timestamp / bus.value) + BigInteger.ONE) * bus.value
+
+fun BigInteger.testValueToTimestamp(withBus: Map.Entry<BigInteger, BigInteger>): BigInteger =
+    this - withBus.key
+
+fun Map<Int, BigInteger>.busWithLargestId(): Map.Entry<BigInteger, BigInteger> = maxByOrNull { it.value }!!.withBigIntKey()
+
+fun Map.Entry<Int, BigInteger>.withBigIntKey() =
+    BusEntry(key.toBigInteger(), value)
+
+class BusEntry(override val key: BigInteger,
+               override val value: BigInteger): Map.Entry<BigInteger, BigInteger>
--- src/test/kotlin/org/tnl/aoc2020/day13test.kt
+++ src/test/kotlin/org/tnl/aoc2020/day13test.kt
@@ -1,11 +1,22 @@
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+import org.tnl.aoc2020.BusEntry
 import org.tnl.aoc2020.Day13Puzzle1
+import org.tnl.aoc2020.Day13Puzzle2
 import org.tnl.aoc2020.calculateWaitingAllTimes
 import org.tnl.aoc2020.calculateWaitingTime
 import org.tnl.aoc2020.findShortestWait
+import org.tnl.aoc2020.isGoldenTimestamp
 import org.tnl.aoc2020.parseBusIds
+import org.tnl.aoc2020.parseBusIdsWithIndex
+import org.tnl.aoc2020.puzzle2BruteForcing
 import org.tnl.aoc2020.readInputs
+import org.tnl.aoc2020.readInputsPuzzle2
+import java.math.BigInteger
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
 
 class Day13Test {
 
@@ -61,6 +72,99 @@ class Day13Test {
 
         // Assert
         assertEquals(295L, result)
+    }
 
+    @Test
+    fun testParseBusIdsWithIndex() {
+        // Act
+        val result = parseBusIdsWithIndex("7,13,x,x,59,x,31,19")
+
+        // Assert
+        assertTrue(result.containsKey(0))
+        assertTrue(result.containsKey(1))
+        assertFalse(result.containsKey(2))
+        assertFalse(result.containsKey(3))
+        assertTrue(result.containsKey(4))
+        assertFalse(result.containsKey(5))
+        assertTrue(result.containsKey(6))
+        assertTrue(result.containsKey(7))
+
+        assertEquals(BigInteger.valueOf(7), result[0])
+        assertEquals(BigInteger.valueOf(13), result[1])
+        assertEquals(BigInteger.valueOf(59), result[4])
+        assertEquals(BigInteger.valueOf(31), result[6])
+        assertEquals(BigInteger.valueOf(19), result[7])
+    }
+
+    @Test
+    fun testReadInputsPuzzle2() {
+        // Act
+        val result = readInputsPuzzle2("day13-test.txt")
+
+        // Assert
+        assertTrue(result.containsKey(0))
+        assertTrue(result.containsKey(1))
+        assertFalse(result.containsKey(2))
+        assertFalse(result.containsKey(3))
+        assertTrue(result.containsKey(4))
+        assertFalse(result.containsKey(5))
+        assertTrue(result.containsKey(6))
+        assertTrue(result.containsKey(7))
+
+        assertEquals(BigInteger.valueOf(7), result[0])
+        assertEquals(BigInteger.valueOf(13), result[1])
+        assertEquals(BigInteger.valueOf(59), result[4])
+        assertEquals(BigInteger.valueOf(31), result[6])
+        assertEquals(BigInteger.valueOf(19), result[7])
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        "1068781, true",
+        "1068788, false",
+        "10681, false",
+        "1068782, false",
+        "1068780, false",
+    )
+    fun testIsGoldenTimestamp(timestamp: Long, expected: Boolean) {
+        // Arrange
+        val buses = parseBusIdsWithIndex("7,13,x,x,59,x,31,19")
+
+        // Act
+        val result = isGoldenTimestamp(BigInteger.valueOf(timestamp), buses)
+
+        // Assert
+        assertEquals(expected, result)
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        "7,13,x,x,59,x,31,19|1068781",
+        "17,x,13,19|3417",
+        "67,7,59,61|754018",
+        "67,x,7,59,61|779210",
+        "67,7,x,59,61|1261476",
+        "1789,37,47,1889|1202161486",
+        delimiter = '|'
+    )
+    fun testBruteForce(inputs: String, expected: Long) {
+        // Arrange
+        val buses = parseBusIdsWithIndex(inputs)
+        val steppingWithBus = BusEntry(BigInteger.ZERO, buses[0]!!)
+
+        // Act
+        val result = puzzle2BruteForcing(BigInteger.ZERO, steppingWithBus, buses)
+
+        // Assert
+        assertEquals(BigInteger.valueOf(expected), result)
+    }
+
+    @Test
+    fun testPuzzle2Answer() {
+        // Act
+        val result = Day13Puzzle2.calculateAnswer("day13-test.txt", BigInteger("1000000"))
+
+        // Assert
+        assertEquals(BigInteger.valueOf(1068781), result)
     }
 }
