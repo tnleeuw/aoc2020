424bb833c388eaae4a40779d99f04e8b49f1b541
Tim van der Leeuw <tim.vanderleeuw@hiber.global>
2020-12-18 23:33:07 +0100

    Day 178 Puzzle 1 - Fix the evaluation, get sum for all answers

--- src/main/kotlin/org/tnl/aoc2020/day18.kt
+++ src/main/kotlin/org/tnl/aoc2020/day18.kt
@@ -2,6 +2,16 @@ package org.tnl.aoc2020
 
 import java.lang.IllegalArgumentException
 
+object Day18Puzzle1 {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val total = sumOfAllAnswers("day18-data.txt")
+
+        println("Total of all answers: ${total}")
+    }
+
+}
+
 sealed class Symbol {
 
     sealed class Term: Symbol() {
@@ -29,16 +39,22 @@ fun parseSymbols(input: Iterator<Char>): Sequence<Symbol> {
                 '*' -> yield(Symbol.Operator.Multiplication)
                 '(' -> yield(Symbol.Term.Expression(parseSymbols(input)))
                 ')' -> return@sequence
-                in '0'..'9' -> yield(parseNumber(chr, input))
+                in '0'..'9' -> {
+                    val (value, nextChar) = parseNumber(chr, input)
+                    yield(value)
+                    // If it's not a closing brace, it's a space and we can ignore it
+                    if (nextChar == ')') return@sequence
+                }
                 else -> throw IllegalArgumentException("Cannot parse character '$chr'")
+
             }
         }
     }
 }
 
-tailrec fun parseNumber(chr: Char, input: Iterator<Char>, accumulator: String = "0"): Symbol.Term.Number = when {
-    chr !in '0'..'9' -> Symbol.Term.Number(accumulator.toLong())
-    !input.hasNext() -> Symbol.Term.Number((accumulator + chr).toLong())
+tailrec fun parseNumber(chr: Char, input: Iterator<Char>, accumulator: String = "0"): Pair<Symbol.Term.Number, Char> = when {
+    chr !in '0'..'9' -> Pair(Symbol.Term.Number(accumulator.toLong()), chr)
+    !input.hasNext() -> Pair(Symbol.Term.Number((accumulator + chr).toLong()), ' ')
     else -> parseNumber(input.next(), input, accumulator + chr)
 }
 
@@ -55,3 +71,10 @@ fun evaluateExpression(symbols: Sequence<Symbol>): Long =
             else -> throw IllegalArgumentException("Cannot evaluate, value: $value, operator: $operator, symbol: $symbol")
         }
     }.first
+
+fun sumOfAllAnswers(fileName: String): Long =
+    getDataInputStream(fileName).bufferedReader()
+        .lineSequence()
+        .map { line -> parseSymbols(line.iterator()) }
+        .map { expression -> evaluateExpression(expression) }
+        .sum()
--- src/test/kotlin/org/tnl/aoc2020/day18test.kt
+++ src/test/kotlin/org/tnl/aoc2020/day18test.kt
@@ -1,6 +1,7 @@
 package org.tnl.aoc2020
 
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
 
@@ -31,5 +32,14 @@ class Day18Test {
 
         // Assert
         assertEquals(expected, result)
+    }
+
+    @Test
+    fun testSumAllAnswers() {
+        // Act
+        val total = sumOfAllAnswers("day18-test.txt")
+
+        // Assert
+        assertEquals(26457L, total)
     }
 }
