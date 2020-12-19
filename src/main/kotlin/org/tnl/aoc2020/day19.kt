970750f95732b19f640b36eba56c28fff5736529
Tim van der Leeuw <tim.vanderleeuw@hiber.global>
2020-12-19 15:12:26 +0100

    Day 19 Puzzle 2: Passes the tests, but produces a final answer that is too high... why?

--- src/main/kotlin/org/tnl/aoc2020/day19.kt
+++ src/main/kotlin/org/tnl/aoc2020/day19.kt
@@ -3,8 +3,11 @@ package org.tnl.aoc2020
 object Day19Puzzle1 {
     @JvmStatic
     fun main(args: Array<String>) {
-        val answer = puzzle1("day19-data.txt")
-        println("Answer puzzle1 = $answer")
+        val answer1 = puzzle1("day19p1-data.txt")
+        println("Answer puzzle1 = $answer1")
+
+        val answer2 = puzzle1("day19p2-data.txt")
+        println("Answer puzzle2 = $answer2")
     }
 
     fun puzzle1(fileName: String): Int {
@@ -14,40 +17,68 @@ object Day19Puzzle1 {
 }
 
 
-sealed class Rule {
-    class ExpandingRule(val expansions: List<Int>) : Rule() {
+sealed class Rule(val idx: Int) {
+    class TerminalRule(idx: Int, val chr: Char) : Rule(idx)
+    class ExpandingRule(idx: Int, val expansions: List<Int>) : Rule(idx) {
         companion object {
-            fun of(ruleData: String) = ExpandingRule(ruleData.trim().split(' ').map { it.toInt() })
+            fun of(idx: Int, ruleData: String) = ExpandingRule(idx, ruleData.trim().split(' ').map { it.toInt() })
+        }
+
+        override fun toString(): String {
+            return "$idx: ${expansions.joinToString(separator = " ")}"
         }
     }
-    class TerminalRule(val chr: Char) : Rule()
-    class OptionRule(val options: List<ExpandingRule>) : Rule()
+    class OptionRule(idx: Int, val options: List<ExpandingRule>) : Rule(idx) {
+        val isRecursive: Boolean = options.any { it.expansions.contains(idx) }
+        override fun toString(): String {
+            return "$idx: ${options.map { it.expansions.joinToString(separator = " ") }.joinToString(separator = "|")}"
+        }
+    }
 }
 
 typealias RuleBase=Map<Int, Rule>
 typealias RuleEntry=Pair<Int, Rule>
 fun parseMessageMatchRule(line: String): RuleEntry {
-    val (idx, ruleData) = line.split(":")
+    val (idxStr, ruleData) = line.split(":")
+    val idx = idxStr.toInt()
     val rule = when {
-        ruleData.contains('"') -> Rule.TerminalRule(ruleData.trim(' ', '"')[0])
-        ruleData.contains('|') -> Rule.OptionRule(ruleData.split('|').map { Rule.ExpandingRule.of(it) })
-        else -> Rule.ExpandingRule.of(ruleData)
+        ruleData.contains('"') -> Rule.TerminalRule(idx, ruleData.trim(' ', '"')[0])
+        ruleData.contains('|') -> Rule.OptionRule(idx, ruleData.split('|')
+            .map { Rule.ExpandingRule.of(-1, it) })
+        else -> Rule.ExpandingRule.of(idx, ruleData)
     }
-    return Pair(idx.toInt(), rule)
+    return Pair(idx, rule)
 }
 
-fun expandRule(rule: Rule, ruleBase: RuleBase): String = when(rule) {
-    is Rule.TerminalRule -> rule.chr.toString()
-    is Rule.ExpandingRule -> {
-        rule.expansions.mapNotNull { ruleBase[it] }
-            .joinToString(separator = "") { expandRule(it, ruleBase) }
+fun expandRule(rule: Rule, ruleBase: RuleBase): String {
+    val result = when (rule) {
+        is Rule.TerminalRule -> rule.chr.toString()
+        is Rule.ExpandingRule -> {
+            rule.expansions.mapNotNull { ruleBase[it] }
+                .joinToString(separator = "") { expandRule(it, ruleBase) }
+        }
+        is Rule.OptionRule -> {
+            if (rule.isRecursive) {
+                // Use special knowledge about the recursive rules: The rules
+                // we have, they loop their first terms
+                rule.options[0].expansions
+                    .mapNotNull { ruleBase[it] }
+                    .map { expandRule(it, ruleBase) }
+                    .joinToString(
+                        separator = "",
+                        prefix = "(", postfix = ")"
+                    ) { "($it)+?" }
+            } else {
+                rule.options.joinToString(
+                    separator = "|",
+                    prefix = "(", postfix = ")"
+                ) { expandRule(it, ruleBase) }
+            }
+        }
     }
-    is Rule.OptionRule -> {
-        rule.options.joinToString(
-            separator = "|",
-            prefix = "(", postfix = ")"
-        ) { expandRule(it, ruleBase) }
-    }
+    if ((rule is Rule.ExpandingRule && rule.idx != -1) || rule is Rule.OptionRule)
+        println("Expansion: $rule -> $result")
+    return result
 }
 
 fun parseRulesAndMessages(fileName: String): Pair<RuleBase, List<String>> {
@@ -56,12 +87,10 @@ fun parseRulesAndMessages(fileName: String): Pair<RuleBase, List<String>> {
 
     var readingRules = true
     getDataInputStream(fileName).bufferedReader().lines().forEach { line ->
-        if (line.isBlank()) {
-            readingRules = false
-        } else if (readingRules) {
-            rules.add(parseMessageMatchRule(line))
-        } else {
-            messages.add(line)
+        when {
+            line.isBlank() -> readingRules = false
+            readingRules -> rules.add(parseMessageMatchRule(line))
+            else -> messages.add(line)
         }
     }
 
@@ -69,6 +98,7 @@ fun parseRulesAndMessages(fileName: String): Pair<RuleBase, List<String>> {
 }
 
 fun matchingMessages(messages: List<String>, ruleBase: RuleBase): List<String> {
-    val ruleRE = expandRule(ruleBase[0]!!, ruleBase).toRegex()
-    return messages.filter { ruleRE.matchEntire(it) != null }
+    val ruleRE = ("^${expandRule(ruleBase[0]!!, ruleBase)}$").toRegex()
+    println("Rule RE: ${ruleRE.pattern}")
+    return messages.filter { ruleRE.matches(it) }
 }
--- src/test/kotlin/org/tnl/aoc2020/day19test.kt
+++ src/test/kotlin/org/tnl/aoc2020/day19test.kt
@@ -1,16 +1,33 @@
 package org.tnl.aoc2020
 
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 import kotlin.test.assertEquals
+import kotlin.test.assertNotEquals
 
 internal class Day19Test {
 
-    @Test
-    fun testPuzzle1() {
+    @ParameterizedTest
+    @CsvSource(
+        "day19p1-test.txt, 2",
+        "day19p2-test1.txt, 3",
+        "day19p2-test2.txt, 12",
+    )
+    fun testPuzzle1(fileName: String, expected: Int) {
         // Act
-        val result = Day19Puzzle1.puzzle1("day19-test.txt")
+        val result = Day19Puzzle1.puzzle1(fileName)
 
         // Assert
-        assertEquals(2, result)
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun testPuzzle2() {
+        // Act
+        val result = Day19Puzzle1.puzzle1("day19p2-data.txt")
+
+        // Assert
+        assertNotEquals(342, result)
     }
 }
